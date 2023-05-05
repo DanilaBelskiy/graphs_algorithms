ac636c6a7fd6e862210f5582283e0b9f643bf228
DanilaBelskiy <belsky32@mail.ru>
2023-05-05 20:34:46 +0300

    main.py changed

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from Johnson import johnson
 from Floyd import floyd
 
 from adjacency_matrices import adjacency_matrix1, adjacency_matrix2, adjacency_matrix3,\
-    adjacency_matrix4, adjacency_matrix5
+    adjacency_matrix4, adjacency_matrix5, adjacency_matrix6, adjacency_matrix7
 
 from work_with_graph import print_graph, print_adjacency_matrix
 
-print_adjacency_matrix(floyd(adjacency_matrix4))
+print_graph(dijkstra(adjacency_matrix4, 2))
