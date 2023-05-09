b1ef4aa333535c35b8a90f13e9c29bcbf9a8786a
DanilaBelskiy <belsky32@mail.ru>
2023-05-09 21:42:44 +0300

    Floyd now work correctly

--- Floyd.py
+++ Floyd.py
@@ -11,7 +11,7 @@ def floyd(adjacency_matrix: list):
     for i in range(len(adjacency_matrix)):
         for j in range(len(adjacency_matrix)):
             for k in range(len(adjacency_matrix)):
-                if adjacency_matrix[i][k] + adjacency_matrix[k][j] < adjacency_matrix[i][j]:
-                    adjacency_matrix[i][j] = adjacency_matrix[i][k] + adjacency_matrix[k][j]
+                if adjacency_matrix[j][i] + adjacency_matrix[i][k] < adjacency_matrix[j][k]:
+                    adjacency_matrix[j][k] = adjacency_matrix[j][i] + adjacency_matrix[i][k]
 
     return adjacency_matrix
